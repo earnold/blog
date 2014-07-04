c83495135ab4b881af89a93d45b15281485f1652
Evan Arnold <evan.arnold@gmail.com>
2014-07-03 17:59:24 -0700

    switch around signaling

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,7 @@
+Chef::Log.info("Stopping Delayed::Jobs")
+sudo "monit -g delayed_job_blog stop"
+sleep 10 #monit and delayed job are slow
+
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 execute "rake assets:precompile" do
@@ -5,7 +9,3 @@ execute "rake assets:precompile" do
   command "bundle exec rake assets:precompile"
   environment "RAILS_ENV" => rails_env
 end
-
-
-Chef::Log.info("Stopping Delayed::Jobs")
-sudo "monit -g delayed_job_blog stop"
